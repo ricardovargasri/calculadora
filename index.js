581b3a98202e69f0c9b84a47baae70606ad4335e
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2023-12-05 22:14:35 -0500

    se agrega borrador de elementos y se optimiza codigo

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ let operators = document.querySelectorAll('.operadores');
 let screen = document.querySelector('.display');
 let firstNumberDisplay = document.querySelector('.firstNumber');
 let lastNumberDisplay = document.querySelector('.lasstNumber');
+let especials = document.querySelectorAll('.especiales');
+let arraiser = document.querySelector('#delete');
 
 //variables
 let operator = '';
 let firstNumber = '';
 let lasstNumber = '';
+let setOperator = true;
 
-//y si todos fuera items con su respectiva subclase? estableciendo sus
-//respectivos if, eslse if sea que se le de click a uno u otro?
+
+
 items.forEach((item) =>{
   
 
@@ -29,67 +32,16 @@ items.forEach((item) =>{
     firstNumber += item.innerHTML;
     firstNumberDisplay.innerHTML = firstNumber;
   }
-   
+
   });
-});
+}
+);
 
+arraiser.addEventListener('click', () =>{
+  let currentContent = firstNumberDisplay.innerHTML;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-document.addEventListener('DOMContentLoaded', function() {
-  //TECLAS DE LA CALCULADORA
-
-  let display = document.querySelector('.display');
-  let reload = document.querySelector('#reload');
-  let deleteButton = document.querySelector('#delete');
-  let uno = document.querySelector('#uno');
-  let dos = document.querySelector('#dos');
-  let tres = document.querySelector('#tres');
-  let cuatro = document.querySelector('#cuatro');
-  let cinco = document.querySelector('#cinco');
-  let seis = document.querySelector('#seis');
-  let siete = document.querySelector('#siete');
-  let ocho = document.querySelector('#ocho');
-  let nueve = document.querySelector('#nueve');
-  let cero = document.querySelector('#cero');
-  let divide = document.querySelector('#divide');
-  let multiply = document.querySelector('#multiply');
-  let add = document.querySelector('#add');
-  let decimal = document.querySelector('#decimal');
-  let equals = document.querySelector('#equals');
-  let menos = document.querySelector('#menos');
-
-  //EVENTOS
-  reload.addEventListener('click', ()=> window.location.reload());
-  deleteButton.addEventListener('click', ()=>{
-    newNumber = display.textContent.slice(0, -1);
-    display.textContent = newNumber;
-  })
-  //numbers
-  uno.addEventListener('click', ()=> display.textContent = display.textContent + uno.innerHTML);
-  dos.addEventListener('click', ()=> display.textContent = display.textContent + dos.innerHTML);
-  tres.addEventListener('click', ()=> display.textContent = display.textContent + tres.innerHTML);
-  cuatro.addEventListener('click', ()=> display.textContent = display.textContent + cuatro.innerHTML);
-  cinco.addEventListener('click', ()=> display.textContent = display.textContent + cinco.innerHTML);
-  seis.addEventListener('click', ()=> display.textContent = display.textContent + seis.innerHTML);
-  siete.addEventListener('click', ()=> display.textContent = display.textContent + siete.innerHTML);
-  ocho.addEventListener('click', ()=> display.textContent = display.textContent + ocho.innerHTML);
-  nueve.addEventListener('click', ()=> display.textContent = display.textContent + nueve.innerHTML);
-  cero.addEventListener('click', ()=> display.textContent = display.textContent + cero.innerHTML);
-}); */
-  
+  if(currentContent.length > 0){
+    let newContent = currentContent.slice(0, -1);
+    firstNumberDisplay.innerHTML = newContent;
+  };
+}); 
